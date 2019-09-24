2bcb5d4bf7075d8250f222ea7b402af5527f67ce
toxema <toxema@gmail.com>
2019-09-24 14:00:49 +0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,59 @@
+<script>
+  export let name;
+  export let data;
+
+  import { onMount } from "svelte";
+  import axios from "axios";
+  let canvasElement;
+
+  function getAllData() {
+    const url = "http://rezonansmuhendislik.com/webcloud/servis.php?cmd=getAll";
+    axios.get(url, {}).then(obj => {
+      data = obj.data;
+      console.log(data);
+    });
+  }
+  onMount(() => {
+    console.log("obj mounted");
+    window.setInterval(() => {
+                    getAllData();
+                }, 3000)
+  });
+</script>
+
+<style>
+  tr.rows:hover {
+    background-color: rgb(214, 255, 253);
+  }
+</style>
+
+<div id="app" class="container">
+  <div class="fix-top bg-dark">
+    <h1 class="text-white">Rezonans DashBoard</h1>
+  </div>
+  <table class="table">
+    <thead class="thead-dark">
+      <tr>
+        <th scope="col">#Dev ID</th>
+        <th scope="col">Name</th>
+        <th scope="col">Tank 1</th>
+        <th scope="col">Tank 2</th>
+        <th scope="col">Firm</th>
+        <th scope="col">Last Update</th>
+      </tr>
+    </thead>
+    <tbody>
+      {#each data as tank}
+        <tr class="rows">
+          <th scope="row">{tank.uid}</th>
+          <td>{tank.name}</td>
+          <td>%{tank.tank1}</td>
+          <td>%{tank.tank2}</td>
+          <td>{tank.firm}</td>
+          <td>{tank.last_update}</td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+
+</div>
---
+++ src/main.js
@@ -0,0 +1,15 @@
+import App from './App.svelte';
+
+const app = new App({
+	target: document.body,
+	props: {
+		name: 'worlds',
+		data : [
+			
+		]
+	},
+  
+});
+
+
+export default app;
